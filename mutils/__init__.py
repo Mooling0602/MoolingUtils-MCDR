a058965b52b2581a0752f6c567a5ef44db7aa382
木泠 <clemooling@outlook.com>
2025-02-06 20:25:55 +0800

    Add decorator execute_if.

--- mutils/__init__.py
+++ mutils/__init__.py
@@ -1,5 +1,17 @@
 from mcdreforged.api.all import *
+from typing import Callable, Any
 
+
+# Usage: @execute_if(bool | Callable -> bool)
+def execute_if(condition: bool | Callable[[], bool]):
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs) -> Any:
+            actual_condition = condition() if callable(condition) else condition
+            if actual_condition:
+                return func(*args, **kwargs)
+            return None
+        return wrapper
+    return decorator
 
 def on_load(server: PluginServerInterface, prev_module):
 # Init MCDR variables.
