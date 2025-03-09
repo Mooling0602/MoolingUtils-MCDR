504bd46dac05a300ed3d8fd56ee442603569445c
木泠 <clemooling@outlook.com>
2025-03-09 19:50:28 +0800

    fix: fix module tr for translates wrongly

--- mutils/tr.py
+++ mutils/tr.py
@@ -1,11 +1,9 @@
 ## This module need MCDR plugin env.
-from . import *
 from typing import Optional
+from mcdreforged.api.types import PluginServerInterface
 
 
-psi = ServerInterface.psi()
-
-def tr(tr_key: str, return_str: Optional[bool] = True):
+def tr(server: PluginServerInterface, tr_key: str, return_str: Optional[bool] = True):
     '''
     [zh_CN]
     对`PluginServerInterface.rtr()`进行优化，提高翻译效率。
@@ -21,17 +19,17 @@ def tr(tr_key: str, return_str: Optional[bool] = True):
     # [zh_CN]
     # 动态获取插件元数据信息，避免解析错误
     # 注意：语言文件的翻译键必须以插件id开头，否则请参考参数`tr_key`的前缀用法
-    plgSelf = psi.get_self_metadata()
+    plgSelf = server.get_self_metadata()
     if tr_key.startswith(f"{plgSelf.id}"):
-        translation = psi.rtr(f"{tr_key}")
+        translation = server.rtr(f"{tr_key}")
     else:
         # [zh_CN]
         # 使用此前缀代表非本插件的翻译键，则翻译时不会附加本插件的ID，避免错误
         # 尚不支持自定义前缀
         if tr_key.startswith("#"):
-            translation = psi.rtr(tr_key.replace("#", ""))
+            translation = server.rtr(tr_key.replace("#", ""))
         else:
-            translation = psi.rtr(f"{plgSelf.id}.{tr_key}")
+            translation = server.rtr(f"{plgSelf.id}.{tr_key}")
     if return_str:
         tr_to_str: str = str(translation)
         return tr_to_str
